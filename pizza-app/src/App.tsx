a9e500e572be7ffa61fba412931b499546f0ce99
yarik.mik <yarik.mik@gmail.com>
2024-06-01 21:38:50 +0700

    up

--- pizza-app/src/App.tsx
+++ pizza-app/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MouseEvent, useState } from 'react';
+import './App.css';
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [counter, setCounter] = useState<number>(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const addCounter = (e: MouseEvent) => {
+		console.log(e);
+	};
+
+	return (
+		<>
+			<Button onClick={addCounter}>Кнопка</Button>
+			<Button appearence='big' onClick={addCounter}>Кнопка</Button>
+			<Input />
+			<Input placeholder='Email' />
+		</>
+	);
 }
 
-export default App
+export default App;
