f64bb58b618fadeab51b224f95d4f09851d474d0
yarik.mik <yarik.mik@gmail.com>
2024-06-14 15:57:46 +0700

    загрузчик

--- pizza-app/src/pages/Menu/Menu.tsx
+++ pizza-app/src/pages/Menu/Menu.tsx
@@ -10,13 +10,22 @@ import axios from 'axios';
 export function Menu() {
 
 	const [products, setProducts] = useState<Product[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 
 	const getMenu = async () =>{
 		try {
+			setIsLoading(true);
+			await new Promise<void>((resolve) => {
+				setTimeout(() => {
+					resolve();
+				}, 2000);
+			});
 			const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
 			setProducts(data);
+			setIsLoading(false);
 		}catch(e) {
 			console.error(e);
+			setIsLoading(false);
 			return;
 		}
 		// try{
@@ -41,7 +50,7 @@ export function Menu() {
 			<Search placeholder='Введите блюдо или состав' />
 		</div>
 		<div>
-			{products.map(p =>(
+			{!isLoading && products.map(p =>(
 				<ProductCard
 					key={p.id}
 					id={p.id}
@@ -52,6 +61,7 @@ export function Menu() {
 					image={p.image}
 				/>
 			))}
+			{isLoading && <>Загружаем продукты...</>}
 
 		</div>
 	</>;
