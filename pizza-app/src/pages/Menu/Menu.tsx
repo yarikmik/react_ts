eb95e4b817e62789eae5efea6ab8d73ca70b87a9
yarik.mik <yarik.mik@gmail.com>
2024-06-14 15:51:48 +0700

    axios

--- pizza-app/src/pages/Menu/Menu.tsx
+++ pizza-app/src/pages/Menu/Menu.tsx
@@ -5,22 +5,30 @@ import Search from '../../components/Search/Search';
 import { PREFIX } from '../../helpers/API';
 import { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
+import axios from 'axios';
 
 export function Menu() {
 
 	const [products, setProducts] = useState<Product[]>([]);
 
 	const getMenu = async () =>{
-		try{
-			const res = await fetch(`${PREFIX}/products`);
-			if (!res.ok) {
-				return;
-			}
-			const data = await res.json() as Product[];
+		try {
+			const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
 			setProducts(data);
-		} catch (e) {
+		}catch(e) {
 			console.error(e);
+			return;
 		}
+		// try{
+		// 	const res = await fetch(`${PREFIX}/products`);
+		// 	if (!res.ok) {
+		// 		return;
+		// 	}
+		// 	const data = await res.json() as Product[];
+		// 	setProducts(data);
+		// } catch (e) {
+		// 	console.error(e);
+		// }
 	};
 
 	useEffect(() => {
