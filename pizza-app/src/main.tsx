fe1f0fb0e6670f66c4bf2363883d00777d392fab
yarik.mik <yarik.mik@gmail.com>
2024-06-18 18:13:36 +0700

    defer Await

--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { RouterProvider, createBrowserRouter} from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, defer} from 'react-router-dom';
 import './index.css';
 import { Cart } from './pages/Cart/Cart.tsx';
 import { Error as ErropPage } from './pages/Error/Error.tsx';
@@ -30,8 +30,13 @@ const router = createBrowserRouter([
 				element: <Product />,
 				errorElement: <>Ошибка</>,
 				loader: async ({ params }) => {
-					const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
-					return data;
+					return defer({
+						data: new Promise((resolve, reject) => {
+							setTimeout(() => {
+								axios.get(`${PREFIX}/products/${params.id}`).then(data => resolve(data)).catch(e => reject(e));
+							}, 2000);
+						})
+					});
 				}
 			}
 		]
