4868303b4636e321c4abcba90a01ac31109ef59e
yarik.mik <yarik.mik@gmail.com>
2024-06-25 12:39:50 +0700

    поиск товаров

--- pizza-app/src/pages/Menu/Menu.tsx
+++ pizza-app/src/pages/Menu/Menu.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-import Headling from '../../components/Headling/Headling';
+import { ChangeEvent, useEffect, useState } from 'react';import Headling from '../../components/Headling/Headling';
 import Search from '../../components/Search/Search';
 import { PREFIX } from '../../helpers/API';
 import { Product } from '../../interfaces/product.interface';
@@ -12,8 +11,13 @@ export default function Menu() {
 	const [products, setProducts] = useState<Product[]>([]);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [error, setError] = useState<string | undefined>();
+	const [filter, setFilter] = useState<string>();
 
-	const getMenu = async () =>{
+	useEffect(() => {
+		getMenu(filter);
+	}, [filter]);
+
+	const getMenu = async (name?: string) => {
 		try {
 			setIsLoading(true);
 			await new Promise<void>((resolve) => {
@@ -21,7 +25,11 @@ export default function Menu() {
 					resolve();
 				}, 500);
 			});
-			const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
+			const { data } = await axios.get<Product[]>(`${PREFIX}/products`, {
+				params: {
+					name
+				}
+			});
 			setProducts(data);
 			setIsLoading(false);
 		}catch(e) {
@@ -44,19 +52,20 @@ export default function Menu() {
 		// }
 	};
 
-	useEffect(() => {
-		getMenu();
-	}, []);
+	const updateFilter = (e: ChangeEvent<HTMLInputElement>) => {
+		setFilter(e.target.value);
+	};
 
 	return <>
 		<div className={styles['head']}>
 			<Headling>Меню</Headling>
-			<Search placeholder='Введите блюдо или состав' />
+			<Search placeholder='Введите блюдо или состав' onChange={updateFilter} />		
 		</div>
 		<div>
 			{error && <>{error}</>}
-			{!isLoading && <MenuList products={products} />}
+			{!isLoading && products.length > 0 && <MenuList products={products} />}			
 			{isLoading && <>Загружаем продукты...</>}
+			{!isLoading && products.length === 0 && <>Не найдено блюд по запросу</>}
 		</div>
 	</>;
 }
