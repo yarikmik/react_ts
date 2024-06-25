99fd7d83f800b7845726c59306b8bc042da08b32
yarik.mik <yarik.mik@gmail.com>
2024-06-25 13:08:27 +0700

    оформление заказа

--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -15,6 +15,7 @@ import { Register } from './pages/Register/Register.tsx';
 import { RequireAuth } from './helpers/RequireAuth.tsx';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
+import { Success } from './pages/Success/Success.tsx';
 
 const Menu = lazy(() => import('./pages/Menu/Menu'));
 
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
 			{
 				path: '/',
 				element: <Suspense fallback={<>Загрузка...</>}><Menu/></Suspense>
+			},
+			{
+				path: '/success',
+				element: <Success />
 			},
 			{
 				path: '/cart',
--- pizza-app/src/pages/Cart/Cart.tsx
+++ pizza-app/src/pages/Cart/Cart.tsx
@@ -1,18 +1,22 @@
-import { useSelector } from 'react-redux';
-import Headling from '../../components/Headling/Headling';
-import { RootState } from '../../store/store';
-import CartItem from '../../components/CartItem/CartItem';
+import { useDispatch, useSelector } from 'react-redux';import Headling from '../../components/Headling/Headling';
+import { AppDispath, RootState } from '../../store/store';import CartItem from '../../components/CartItem/CartItem';
 import { useEffect, useState } from 'react';
 import { Product } from '../../interfaces/product.interface';
 import axios from 'axios';
 import { PREFIX } from '../../helpers/API';
 import styles from './Cart.module.css';
+import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
+import { cartActions } from '../../store/cart.slice';
 
 const DELIVERY_FEE = 169;
 
 export function Cart() {
 	const [cartProducts, setCardProducts] = useState<Product[]>([]);
 	const items = useSelector((s: RootState) => s.cart.items);
+	const jwt = useSelector((s: RootState) => s.user.jwt);
+	const dispatch = useDispatch<AppDispath>();
+	const navigate = useNavigate();
 
 	const total = items.map(i => {
 		const product = cartProducts.find(p => p.id === i.id);
@@ -25,6 +29,18 @@ export function Cart() {
 	const getItem = async (id: number) => {
 		const { data } = await axios.get<Product>(`${PREFIX}/products/${id}`);
 		return data;
+	};
+
+	const checkout = async () => {
+		await axios.post(`${PREFIX}/order`, {
+			products: items
+		}, {
+			headers: {
+				Authorization: `Bearer ${jwt}`
+			}
+		});
+		dispatch(cartActions.clean());
+		navigate('/success');
 	};
 
 	const loadAllItems = async () => {
@@ -55,8 +71,10 @@ export function Cart() {
 		</div>
 		<hr className={styles['hr']} />
 		<div className={styles['line']}>
-			<div className={styles['text']}>Итог {items.length}</div>
-			<div className={styles['price']}>{total + DELIVERY_FEE}&nbsp;<span>₽</span></div>
+			<div className={styles['text']}>Итог <span className={styles['total-count']}>({items.length})</span></div>			<div className={styles['price']}>{total + DELIVERY_FEE}&nbsp;<span>₽</span></div>
+		</div>
+		<div className={styles['checkout']}>
+			<Button appearence="big" onClick={checkout}>оформить</Button>
 		</div>
 	</>;
 }
